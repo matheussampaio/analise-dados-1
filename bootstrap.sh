75e2cb5bef757df666706c1f680a70258ea0573b
Matheus Sampaio <matheus1401@gmail.com>
2014-05-12 14:56:11 -0300

    Fixed installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,3 +4,10 @@ apt-get update
 
 # Instalando Dependencias:
 apt-get install -y g++ curl libssl-dev apache2-utils unzip make gcc make build-essential
+
+wget http://nbcgib.uesc.br/mirrors/cran/bin/linux/ubuntu/precise/r-base_3.1.0.orig.tar.gz
+tar -xf r-base_3.1.0.orig.tar.gz
+cd R-3.1.0/
+
+./configure --with-x=no
+make
