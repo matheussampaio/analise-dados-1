4f0ea6dfedc71fc4553f364265a00c4bfcb13864
Matheus Sampaio <matheus1401@gmail.com>
2014-05-12 15:37:45 -0300

    Add r-base-core

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 apt-get update
 
 # Instalando Dependencias:
-apt-get install -y g++ curl libssl-dev apache2-utils unzip make gcc make build-essential
+apt-get install -y g++ curl libssl-dev apache2-utils unzip make gcc make build-essential r-base-core
 
 wget http://nbcgib.uesc.br/mirrors/cran/bin/linux/ubuntu/precise/r-base_3.1.0.orig.tar.gz
 tar -xf r-base_3.1.0.orig.tar.gz
