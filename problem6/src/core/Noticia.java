ff598f42b7620d2289ab654ea128e2823d26b7d1
Matheus Sampaio <matheus1401@gmail.com>
2014-08-13 17:50:01 -0300

    init problem6

---
+++ problem6/src/core/Noticia.java
@@ -0,0 +1,8 @@
+
+package core;
+
+public class Noticia {
+
+    public Noticia() {
+    };
+}
---
+++ problem6/src/problem6/Main.java
@@ -0,0 +1,27 @@
+
+package problem6;
+
+public class Main {
+
+    //    Lab06_MineracaoDeTexto() {
+    //        empresas = realizarLeituraDeDadosDasEmpresas();
+    //        // irei trabalhar com nome da empresa? como nome do pregão? com ambas?
+    //        // precisarei de um nome mais legível para empresa?
+    //        // essa será a primeira decisão da análise de dados que você deverá
+    //        // tomar...
+    //        for (final Empresa e : empresas) {
+    //            // Onde na Web? google.com? news.google? reuters? infomoney? Por
+    //            // que?
+    //            páginas = consultarWeb(e);
+    //            for (final Pagina p : paginas) {
+    //                // guardar título, subtítulo(quanto houver) e texto principal.
+    //                tratarHTML(p);
+    //            }
+    //        }
+    //        // Como relacionar as empresas por meio dos textos das notícias? TF-IDF?
+    //        // weka? alguma outra sugestão?
+    //        grupos = minerarTexto();
+    //        return grupos;
+    //    }
+
+}
